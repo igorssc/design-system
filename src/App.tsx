224f43428ea729ea45e9f02a9d5e1ff6ef15afb6
Igor Santos <igorsantoscosta@gmail.com>
2022-10-13 19:05:23 -0300

    Add storybook workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "../src/styles/global.css";
-import { TextInput } from "./components/TextInput";
 
 export function App() {
   return (
     <>
       <h1 className="text-cyan-500">Hello world</h1>
-      <TextInput.Root>
-        <TextInput.Input placeholder="ola" />
-      </TextInput.Root>
     </>
   );
 }
